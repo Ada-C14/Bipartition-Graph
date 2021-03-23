dddc231f43e25d4a0d7ab78fbf26b5417a3abb09
PaulineChane <pauline.chane@gmail.com>
2021-03-23 01:59:43 -0700

    time/space complexity updated

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,6 +1,14 @@
 
-# Time Complexity -
-# Space Complexity - 
+# Time Complexity - O(v + e) Using BFS, the function checks each vertex through the queue once, and all edges 
+# associated with that queue once or twice at most. Checking for non biparition-able graphs via edges beforehand
+# and only adding to the queue any edges with vertices that haven't been visited as a vertex prevents
+# repeat vertices from being visited. 
+
+# Space Complexity - O(v) - A hash, and set, and a queue are created where the max number of elements added will never exceed
+# the number of vertices (v) in the graph. group_hash will always have two keys between which the vertices are divided into based on 
+# edges, to_visit is a set of all vertices to delete from (in O(1) time), while bfs at worst will have all vertices in the Queue,
+# as the function doesn't add any edges that have already been visited. to_visit mostly exists to track any vertices 
+# in a graph that aren't reached by a group of other vertices in the graph, which group_hash and bfs_q can't account for. 
 def possible_bipartition(dislikes)
   return true if dislikes.empty?
   
@@ -20,13 +28,11 @@ def possible_bipartition(dislikes)
   to_visit.delete(0)
 
   dislikes[0].each do |e|
-    group_hash[e_group].add(e)
+    group_hash[e_group].add(e) unless e == 0 
     bfs_q << e
   end
 
-
   until to_visit.empty? 
-    
     until bfs_q.empty?
       # figure out where to put cur_v
       cur_v = bfs_q.pop 
@@ -52,7 +58,7 @@ def possible_bipartition(dislikes)
     # bfs ensures that any vertices left in to_visit won't interact with those currently in groups
     # we can just grab the first element in the to_visit set
     if !to_visit.empty?
-      bfs_q << to_visit.first
+      bfs_q << to_visit.first # for a general set, we can assume that set pulls any number it wants out, we don't care which one
     end
 
   end
