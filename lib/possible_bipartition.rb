cbe7db224401e858ae2763dc06b32b0a7f4a4538
dnsrocha <drocha@gmail.com>
2021-05-13 17:08:41 -0700

    addeedd possible_bipartition

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,31 @@
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  dog_group1 = []
+  dog_group2 = []
+
+  dislikes.each_with_index do |enemies, dog|
+    safe1 = true
+    safe2 = true
+    enemies.each do |enemy|
+      if dog_group1.include?(enemy)
+        safe1 = false
+      elsif dog_group2.include?(enemy)
+        safe2 = false
+      end
+    end
+      
+    if safe1
+      dog_group1 << dog
+      #dog_group2 << enemy
+    elsif safe2
+      dog_group2 << dog
+    else
+      return false
+    end
+  end
+  return true
 end
+
+
+
+#breadth first solution
