84b0a2ed0951f567b52d24e14a4b8377e90638c4
beauttie <15842537+beauttie@users.noreply.github.com>
2021-05-20 00:31:26 -0700

    Draft of possible_bipartition

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,23 @@
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  return true if dislikes.empty? 
+
+  group_num = Array.new(dislikes.length)
+  group_num[0] = 1
+  queue = Queue.new
+  queue.enq(0)
+  while !queue.empty?
+    current = queue.deq
+    dislikes[current].each do |dog|
+      if group_num[dog].nil?
+        group_num[dog] = group_num[current] == 1 ? 2 : 1
+        queue.enq(dog)
+      elsif group_num[dog] == group_num[current]
+        return false
+      end
+    end
+    queue.enq(current + 1) unless current == dislikes.length - 1
+  end
+
+  return true
 end
