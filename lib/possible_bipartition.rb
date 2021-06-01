f59dfba2abce218b6c9db7a283472a8942b5b958
Kimberly Vitug <kim_vitug05@yahoo.com>
2021-05-31 21:04:32 -0700

    add solution for possible bipartition

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,36 @@
+# Time Complexity: O(n x m), where n is the number of puppies, m is the number of enemies
+# Space Complexity:
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  puppy_enemies = {}
+
+  dislikes.each_with_index do |puppies, index|
+    puppy_enemies[index] = puppies
+  end
+
+  puppy_groups = {}
+
+  dislikes.length.times do |puppy|
+    next if puppy_groups[puppy] # skip if group is already set
+
+    puppy_groups[puppy] = 'RED'
+
+    q = Queue.new
+    q.enq(puppy)
+
+    until q.empty?
+      current = q.deq
+
+      puppy_enemies[current].each do |enemy|
+        if puppy_groups[enemy].nil?
+          puppy_groups[enemy] = puppy_groups[current] == 'RED' ? 'BLUE' : 'RED'
+          q.enq(enemy)
+        elsif puppy_groups[enemy] == puppy_groups[current]
+          return false
+        end
+      end
+    end
+  end
+
+  return true
 end
