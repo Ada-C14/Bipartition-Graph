6477f977c44fddeb25690633544be77a6a99841d
Ringo Alcock <ringo.alcock@gmail.com>
2021-06-14 19:37:07 -0700

    define possible_bipartition() method

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,35 @@
+# Time complexity: in a breadth first search, each node (N) must be visited
+# and each edge (E) must be checked, so the time involved is O(N+E)
+# Space complexity: in the worst case scenario, q will contain every node
+# in dislikes, so space is O(N)
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  q = Queue.new
+  visited = Array.new(dislikes.length, false)
+  group = Array.new(dislikes.length)
+  i = 0
+
+  while visited.include?(false)
+    group[i] = 1
+    visited[i] = 1
+    q.push(i)
+
+    until q.empty?
+      current = q.pop()
+      dislikes[current].each do |dog|
+        if visited[dog]
+          if group[dog] == group[current]
+            return false
+          end
+        else
+          q.push(dog)
+          visited[dog] = true
+          group[dog] = -group[current]
+        end
+      end
+    end
+    i = visited.index(false)
+  end
+
+  return true
 end
